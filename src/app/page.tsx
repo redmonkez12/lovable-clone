c060a749dc53943649a1d16dd6f01e6e0974ce97
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-07-01 09:50:02 +0200

    Connect anthropic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import { Button } from "@/components/ui/button";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
+import {useState} from "react";
+import {Input} from "@/components/ui/input";
 
 export default function Home() {
+    const [value, setValue] = useState("");
+
     const trpc = useTRPC();
     const invoke = useMutation(trpc.invoke.mutationOptions({
         onSuccess() {
@@ -14,8 +18,9 @@ export default function Home() {
     }));
     return (
         <div className="p-4 max-w-7xl mx-auto">
+            <Input value={value} onChange={(e) => setValue(e.target.value)}/>
             <Button disabled={invoke.isPending}
-                    onClick={() => invoke.mutate({ text: "John" })}>Invoke Background
+                    onClick={() => invoke.mutate({ value })}>Invoke Background
                 Job</Button>
         </div>
     );
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,25 @@
 import { inngest } from "./client";
+import {createAgent} from "@inngest/agent-kit";
+import {anthropic} from "inngest";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
     { event: "test/hello.world" },
-    async ({ event, step }) => {
-        await step.sleep("wait-a-moment", "1s");
-        return { message: `Hello ${event.data.email}!` };
+    async ({ event }) => {
+        const codeAgent = createAgent({
+            name: "codeAgent",
+            system: "You are an expert next.js developer. You are readable, maintainable code. You write simple Next.js & React snippets.",
+            model: anthropic({
+                model: "claude-3-5-haiku-20241022", apiKey: process.env.ANTHROPIC_API_KEY,
+                defaultParameters: {
+                    max_tokens: 1000,
+                    temperature: 0.7,
+                }
+            }),
+        });
+
+        const { output } = await codeAgent.run(`Write the following snippet: ${event.data.value}`);
+
+        return { output };
     },
 );
